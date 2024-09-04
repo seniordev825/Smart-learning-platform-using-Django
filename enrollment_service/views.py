01d38d6ac2c533ff77d0fefbf3113a86b11fec1c
Bright <134352369+seniordev825@users.noreply.github.com>
2024-09-04 10:02:36 +0000

    Update views.py

--- enrollment_service/views.py
+++ enrollment_service/views.py
@@ -8,7 +8,7 @@ from rest_framework import (
     validators,
 )
 from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.response import Response 
 from rest_framework import status
 from rest_framework import permissions
 from course_service.models import Course
