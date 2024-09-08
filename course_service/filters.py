bf9a51659e02358be49d8cf917b399331a9ac886
Bright <134352369+seniordev825@users.noreply.github.com>
2024-09-08 02:20:18 +0000

    Update filters.py

--- course_service/filters.py
+++ course_service/filters.py
@@ -6,6 +6,6 @@ class CourseFilter(django_filters.FilterSet):
     instructor_first_name = django_filters.CharFilter(field_name="instructor__user__first_name", lookup_expr="icontains")
     instructor_last_name = django_filters.CharFilter(field_name="instructor__user__last_name", lookup_expr="icontains")
     
-    class Meta:
+    class Meta: 
         model = Course 
         fields = ["instructor_first_name", "instructor_last_name", "difficulty", "is_certified", "reviews", "price", "is_available"]
